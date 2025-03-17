26de114d7e461b78b2c891e37eaf8f354fe06c46
precious okiemen <okiemenprecious@gmail.com>
2025-03-17 22:46:18 +0100

    feat(terraform): add Atlantis external secrets management - Add - Add - Add

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -12,3 +12,31 @@ resource "aws_iam_role" "ec2_role" {
     }]
   })
 }
+
+
+
+# IAM Policy to access AWS Secrets Manager
+resource "aws_iam_policy" "secrets_manager_policy" {
+  name   = "secrets-manager-access"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:DescribeSecret"
+        ]
+        Resource = "arn:aws:secretsmanager:us-east-1:842676016628:secret:atlantis-ssh-key-SDowhJ"  # replace with your own arn
+      }
+    ]
+  })
+}
+
+
+# Attach the policy to the IAM role
+resource "aws_iam_role_policy_attachment" "attach_secrets_policy" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.secrets_manager_policy.arn
+}
+
